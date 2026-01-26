a8f5d49e0af8062c1d5b55a379250a4671c1efc7
daparamadhan <daparamdnnn@gmail.com>
2026-01-26 13:16:10 +0700

    Three Comm

--- WasteManagementSystem/obj/Debug/net9.0-windows/WasteManagementSystem.AssemblyInfo.cs
+++ WasteManagementSystem/obj/Debug/net9.0-windows/WasteManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WasteManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d728c4abcf17b2a6f933038eac67ab64dc4d687")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f322e8fa3c91df2d90a62585f9cadf7d1c32c1a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WasteManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WasteManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
